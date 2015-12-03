a7512555b38e186a4b8b6df74897996d420e163c
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-12-03 12:51:49 +0400

    checkout can scan item doubles and add to basket

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,8 +2,14 @@ require_relative './promotional_rules.rb'
 
 class Checkout
 	attr_reader :promotional_rules
+	attr_reader :basket
 
 	def initialize(promotional_rules = PromotionalRules.new)
 		@promotional_rules = promotional_rules
+		@basket = []
+	end
+
+	def scan item
+		basket << item
 	end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,8 +1,15 @@
 require 'checkout'
 
 describe 'Checkout' do
+	let(:co){ Checkout.new }
+	let(:lavender_heart){ double :item, title: "Lavender Heart", product_code: 001, price: 9.25 }
+
 	it 'initializes with promotional rules by default' do
-		c = Checkout.new
-		expect(c.promotional_rules).to be_a PromotionalRules
+		expect(co.promotional_rules).to be_a PromotionalRules
+	end
+
+	it 'can scan an item' do
+		co.scan(lavender_heart)
+		expect(co.basket).to include lavender_heart
 	end
 end
