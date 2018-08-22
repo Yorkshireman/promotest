60d07230d2d994c86dd671e85441d6f69eaa59cc
Andrew Stelmach <Yorkshireman@users.noreply.github.com>
2018-08-22 11:34:55 +0100

    Update product_range.rb

--- lib/product_range.rb
+++ lib/product_range.rb
@@ -1,10 +1,10 @@
 module ProductRange
   def self.items
     { 
-    001 => { product: "Lavender Heart", price: 9.25 }, 
-    002 => { product: "Personalised cufflinks", price: 45.00 },
-    003 => { product: "Kids T-Shirt", price: 19.95 },
-    004 => { product: "Cinnamon Shampoo", price: 8.99 } 
+      001 => { product: "Lavender Heart", price: 9.25 }, 
+      002 => { product: "Personalised cufflinks", price: 45.00 },
+      003 => { product: "Kids T-Shirt", price: 19.95 },
+      004 => { product: "Cinnamon Shampoo", price: 8.99 } 
     }
   end
 end
